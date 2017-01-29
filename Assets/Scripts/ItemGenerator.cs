80f986896f211825cf4bad99cf51f4721c75765d
Ashidariku <lirckm@gmail.com>
2017-01-29 23:12:31 +0900

    StepAsideUnityChanプロジュエクト　更新
    発展課題終了。

--- Assets/Scripts/ItemGenerator.cs
+++ Assets/Scripts/ItemGenerator.cs
@@ -25,7 +25,14 @@ public class ItemGenerator : MonoBehaviour {
 
 	// Prefabs
 	private List<GameObject> prefabList = new List<GameObject>();
-
+	// 画面外の距離
+	private float destroyDistance = 10;
+	// アイテム範囲
+	private int itemRange = 50;
+	// アイテム設置終了位置
+	private float endRangeLog;
+	// アイテム設置終了位置から40m手前
+	private float startLine;
 
 	// Use this for initialization
 	void Start () {
@@ -35,8 +42,57 @@ public class ItemGenerator : MonoBehaviour {
 		// Unityちゃんの位置を取得
 		this.positionZ = unitychan.transform.position.z;
 
+		// 初期障害物を設置
+		GenerateItems (this.startPos, this.startPos + itemRange);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Unityちゃんの位置を取得
+		this.positionZ = this.unitychan.transform.position.z;
+
+		// Unityちゃんがアイテムを設置してある40m手前のLineを超える
+		if ((this.positionZ > this.startLine) && (this.endRangeLog < this.goalPos)) {
+			// Unityちゃんの前方40-50mの範囲に障害物を設置
+			GenerateItems (this.endRangeLog, this.endRangeLog + 10);
+		}
+			
+		// 障害物を削除
+		DestroyItems ();
+
+		// Debug.Log
+		//Debug.Log (this.positionZ + "," +this.startLine + "," + this.endRangeLog);
+	}
+
+	// unityちゃんが通り過ぎた障害物を削除
+	void DestroyItems(){
+
+		// 全ての障害物
+		for (int i = 0; i < prefabList.Count; i++) {
+			
+			// prefabListの中身を代入
+			GameObject prefabObject = prefabList [i];
+
+			// Unityちゃんの座標より後ろになり、画面外いなると
+			if (prefabObject.transform.position.z + destroyDistance < this.positionZ) {
+				// prefabListから削除
+				this.prefabList.Remove (prefabObject);
+				// prefabObjectを削除
+				Destroy (prefabObject);
+			}
+		}
+	}
+
+	// アイテム生成（アイテム設置開始、アイテム設置終了）
+	void GenerateItems(float startRange, float endRange){
+
+		// アイテムを設置した位置の更新
+		this.endRangeLog = endRange;
+		this.startLine = endRange - 40;
+
 		// 一定の距離ごとにアイテムを生成
-		for (int i = startPos; i < goalPos; i += 15) {
+		for (float i = startRange; i < endRange; i += 15) {
 			// どのアイテムを出すのかをランダムに設定
 			int num = Random.Range (0, 10);
 			if (num <= 1) {
@@ -71,32 +127,6 @@ public class ItemGenerator : MonoBehaviour {
 						prefabList.Add (car);
 					}
 				}
-			}
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-
-		// 障害物を削除
-		DestroyItems ();
-	}
-
-	void DestroyItems(){
-		// Unityちゃんの位置を取得
-		this.positionZ = this.unitychan.transform.position.z;
-
-		// 全ての障害物
-		for (int i = 0; i < prefabList.Count; i++) {
-			GameObject prefabObject = prefabList [i];
-
-			// Unityちゃんの座標より後ろになると
-			if (prefabObject.transform.position.z < this.positionZ) {
-				// prefabListから削除
-				this.prefabList.Remove (prefabObject);
-				// prefabObjectを削除
-				Destroy (prefabObject);
 			}
 		}
 	}
